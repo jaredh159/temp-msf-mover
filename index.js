7365293b10b80483254a0a3052408a6b4b7f4740
Jared Henderson <jared@netrivet.com>
2021-08-20 14:51:59 -0400

    SAVEPOINT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require(`dotenv`).load();
 const { red, green, magenta, yellow, c, log } = require(`x-chalk`);
 const { sync: glob } = require(`glob`);
+const { uploadFile } = require(`@friends-library/cloud`);
 
 const rootDir = process.env.ROOT_DIR;
 
@@ -9,9 +10,11 @@ const allFiles = glob(`${rootDir}/**/*.{epub,mobi,pdf,mp3}`, { nodir: true });
 
 log(c`{gray starting transfer of} {cyan ${allFiles.length}} {gray files}`);
 
-for (const path of allFiles) {
-  const relpath = path.replace(`${rootDir}/`, ``);
-  const cloudPath = `storage/msf-site/assets/${relpath}`;
-  log(c`starting transfer of file {magenta ${relpath}}`);
-  console.log({ localPath: path, cloudPath });
+async function main() {
+  for (const path of allFiles) {
+    const relpath = path.replace(`${rootDir}/`, ``);
+    const cloudPath = `storage/msf-site-backup/assets/${relpath}`;
+    log(c`starting transfer of file {magenta ${relpath}}`);
+    await uploadFile(path, cloudPath);
+  }
 }
